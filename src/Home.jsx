9ea0ababf40ea2f9e0c7fd5608926b3a3c77e912
gokul-vsi <gokulnath2923@gmail.com>
2024-12-07 15:17:41 +0530

    remove br

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,7 +13,7 @@ export const Home = () => {
           <div className="col-md-6 col-lg-6 order-1 order-md-2 mt-md-5 home-2">
             <h1 className='mt-5 ms-md-5 ms-lg-5'>Hello, I'm Gokulnath</h1>
              <AutoType/>
-             <p className='ms-md-5 ms-lg-5 mt-3 home-p w-100'>Specialized in MERN Stack, I build dynamic, user-friendly web applications that look great and work flawlessly across all devices. <br /> Let's bring your vision to life with clean code and compelling design.</p>
+             <p className='ms-md-5 ms-lg-5 mt-3 home-p w-100'>Specialized in MERN Stack, I build dynamic, user-friendly web applications that look great and work flawlessly across all devices. Let's bring your vision to life with clean code and compelling design.</p>
              <div className='d-flex gap-1'>
              <button className='ms-md-5 ms-lg-5 mt-3  home1-btn'>Contact Me</button>
              <div className='animate__animated animate__pulse animate__slow animate__infinite'><button className='ms-md-5 ms-lg-5 mt-3 home2-btn'>Hire Me</button></div>
