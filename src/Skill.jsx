494c2d99cfd762cdcd145fa397d9fe8bff335c67
gokul-vsi <gokulnath2923@gmail.com>
2024-12-13 14:21:27 +0530

    skills-changes

--- src/Skill.jsx
+++ src/Skill.jsx
@@ -32,7 +32,7 @@ export const Skill = () => {
               </div>         
           </div>
           <div className="col-md-6 col-lg-6">
-            <div className="second-main d-flex gap-5 ms-md-5">
+            <div className="second-main d-flex gap-5 ms-md-5 skill-mobile">
             <div className="second1 d-flex flex-column gap-2">
             <h5><MdVerified  className='verified' /> Figma</h5>
             <h5><MdVerified  className='verified' /> Wordpress</h5>
@@ -40,7 +40,7 @@ export const Skill = () => {
             <h5><MdVerified  className='verified' /> AOS</h5>
             <h5><MdVerified  className='verified' /> Git</h5>
             </div>
-            <div className="second2 d-flex flex-column gap-2">
+            <div className="second2 d-flex flex-column gap-2 sk-mob">
             <h5><MdVerified  className='verified' /> Github</h5>
             <h5><MdVerified  className='verified' /> Django</h5>
             </div>
