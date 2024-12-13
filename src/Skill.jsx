07ab3aa44fcdc9659fd7e03f631ff83ae6105474
gokul-vsi <gokulnath2923@gmail.com>
2024-12-13 15:18:37 +0530

    skills-font

--- src/Skill.jsx
+++ src/Skill.jsx
@@ -16,34 +16,34 @@ export const Skill = () => {
           <div className="col-md-6 col-lg-6">
              <div className="first-main d-flex gap-5 ms-md-5">
              <div className="first-1 d-flex flex-column gap-2 sk-mon-1">
-              <h5><MdVerified  className='verified' /> HTML</h5>
-              <h5><MdVerified  className='verified' /> CSS</h5>
-              <h5><MdVerified  className='verified' /> Javascript</h5>
-              <h5><MdVerified  className='verified' /> Bootstrap</h5>
-              <h5><MdVerified  className='verified' /> Tailwind CSS</h5>
+              <h5 className='sk-h'><MdVerified  className='verified' /> HTML</h5>
+              <h5 className='sk-h'><MdVerified  className='verified' /> CSS</h5>
+              <h5 className='sk-h'><MdVerified  className='verified' /> Javascript</h5>
+              <h5 className='sk-h'><MdVerified  className='verified' /> Bootstrap</h5>
+              <h5 className='sk-h'><MdVerified  className='verified' /> Tailwind CSS</h5>
              </div>
              <div className="first-2 d-flex flex-column gap-2">
-             <h5><MdVerified  className='verified' /> React</h5>
-             <h5><MdVerified  className='verified' /> Node.js</h5>
-             <h5><MdVerified  className='verified' /> Express.js</h5>
-             <h5><MdVerified  className='verified' /> MongoDB</h5>
-             <h5><MdVerified  className='verified' /> JQuery</h5>
+             <h5 className='sk-h'><MdVerified  className='verified' /> React</h5>
+             <h5 className='sk-h'><MdVerified  className='verified' /> Node.js</h5>
+             <h5 className='sk-h'><MdVerified  className='verified' /> Express.js</h5>
+             <h5 className='sk-h'><MdVerified  className='verified' /> MongoDB</h5>
+             <h5 className='sk-h'><MdVerified  className='verified' /> JQuery</h5>
               </div>    
               </div>         
           </div>
           <div className="col-md-6 col-lg-6">
             <div className="second-main d-flex gap-5 ms-md-5 skill-mobile">
             <div className="second1 d-flex flex-column gap-2">
-            <h5><MdVerified  className='verified' /> Figma</h5>
-            <h5><MdVerified  className='verified' /> Wordpress</h5>
-            <h5><MdVerified  className='verified' /> Firebase</h5>
-            <h5><MdVerified  className='verified' /> AOS</h5>
-            <h5><MdVerified  className='verified' /> Git</h5>
+            <h5 className='sk-h'><MdVerified  className='verified' /> Figma</h5>
+            <h5 className='sk-h'><MdVerified  className='verified' /> Wordpress</h5>
+            <h5 className='sk-h'><MdVerified  className='verified' /> Firebase</h5>
+            <h5 className='sk-h'><MdVerified  className='verified' /> AOS</h5>
+            <h5 className='sk-h'><MdVerified  className='verified' /> Git</h5>
             </div>
             <div className="second2 d-flex flex-column gap-2 sk-mob">
-            <h5><MdVerified  className='verified' /> Github</h5>
-            <h5><MdVerified  className='verified' /> Django</h5>
-            <h5><MdVerified  className='verified' /> Redux</h5>
+            <h5 className='sk-h'><MdVerified  className='verified' /> Github</h5>
+            <h5 className='sk-h'><MdVerified  className='verified' /> Django</h5>
+            <h5 className='sk-h'><MdVerified  className='verified' /> Redux</h5>
             </div>
             </div>
             </div>
