48abd820e2192f900fa34a95c931b556ab4317c3
gokul-vsi <gokulnath2923@gmail.com>
2024-12-15 18:04:52 +0530

    images

--- src/Skill.jsx
+++ src/Skill.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 import { MdVerified } from "react-icons/md";
+import html from './images/html.png'
+import css from './images/css.png'
+import js from './images/js.png'
+import react from './images/react.png'
+import nodejs from './images/nodejs1.png'
+import express from './images/express.png'
+import mongo from './images/mongodb.png'
+import firebase from './images/fireabase.png'
+import redux from './images/redux.png'
+import git from './images/git.png'
+import figma from './images/figma.webp'
 
 export const Skill = () => {
   return (
@@ -11,44 +22,97 @@ export const Skill = () => {
           </div>
         </div>
       </div>
-       <div className="container skill">
+      <div className="container mt-5 mb-md-5">
         <div className="row">
-          <div className="col-md-6 col-lg-6">
-             <div className="first-main d-flex justify-content-center align-item-center gap-5 ms-md-3">
-             <div className="first-1 d-flex flex-column gap-2 sk-mon-1">
-              <h5 className='sk-h'><MdVerified  className='verified' /> HTML</h5>
-              <h5 className='sk-h'><MdVerified  className='verified' /> CSS</h5>
-              <h5 className='sk-h'><MdVerified  className='verified' /> Javascript</h5>
-              <h5 className='sk-h'><MdVerified  className='verified' /> Bootstrap</h5>
-              <h5 className='sk-h'><MdVerified  className='verified' /> Tailwind CSS</h5>
-             </div>
-             <div className="first-2 d-flex flex-column gap-2">
-             <h5 className='sk-h'><MdVerified  className='verified' /> React</h5>
-             <h5 className='sk-h'><MdVerified  className='verified' /> Node.js</h5>
-             <h5 className='sk-h'><MdVerified  className='verified' /> Express.js</h5>
-             <h5 className='sk-h'><MdVerified  className='verified' /> MongoDB</h5>
-             <h5 className='sk-h'><MdVerified  className='verified' /> JQuery</h5>
-              </div>    
-              </div>         
+        <div className="col-md-3">
+           <div className="className='w-50 h-100 skill-box">
+             <img src={html} className='skill-width1 mx-auto d-block img-fluid' alt="" />
+             <h5 className='text-center mt-3'>HTML</h5>
+             <p className='text-center skill-p'>Building structured web content</p>
+           </div>
           </div>
-          <div className="col-md-6 col-lg-6">
-            <div className="second-main d-flex justify-content-center align-item-center gap-5 ms-md-5 skill-mobile">
-            <div className="second1 d-flex flex-column gap-2">
-            <h5 className='sk-h'><MdVerified  className='verified' /> Figma</h5>
-            <h5 className='sk-h'><MdVerified  className='verified' /> Wordpress</h5>
-            <h5 className='sk-h'><MdVerified  className='verified' /> Firebase</h5>
-            <h5 className='sk-h'><MdVerified  className='verified' /> AOS</h5>
-            <h5 className='sk-h'><MdVerified  className='verified' /> Git</h5>
-            </div>
-            <div className="second2 d-flex flex-column gap-2 sk-mob">
-            <h5 className='sk-h'><MdVerified  className='verified' /> Github</h5>
-            <h5 className='sk-h'><MdVerified  className='verified' /> Django</h5>
-            <h5 className='sk-h'><MdVerified  className='verified' /> Redux</h5>
-            </div>
-            </div>
-            </div>
+          <div className="col-md-3">
+           <div className="className='w-50 h-100 skill-box">
+             <img src={css} className='skill-width mx-auto d-block img-fluid mt-md-3' alt="" />
+             <h5 className='text-center mt-3'>CSS</h5>
+             <p className='text-center text-sm skill-p'>Styling and layout for modern web designs</p>
+           </div>
+          </div>
+          <div className="col-md-3">
+          <div className="className='w-50 h-100 skill-box">
+             <img src={js} className='skill-width mx-auto  d-block img-fluid mt-md-3' alt="" />
+             <h5 className='text-center mt-3'>Javascript</h5>
+             <p className='text-center text-sm skill-p'>Building interactive and dynamic web applications</p>
+           </div>
+          </div>
+          <div className="col-md-3">
+          <div className="className='w-50 h-100 skill-box">
+             <img src={react} className='skill-width mx-auto d-block img-fluid mt-md-3' alt="" />
+             <h5 className='text-center mt-3'>React</h5>
+             <p className='text-center text-sm skill-p'>Building interactive UIs with state management</p>
+           </div>
+          </div>
         </div>
-       </div>
+      </div>
+      <div className="container mb-md-5">
+        <div className="row">
+          <div className="col-md-3">
+          <div className="className='w-50 h-100 skill-box">
+             <img src={nodejs} className='skill-width mx-auto d-block img-fluid mt-md-3' alt="" />
+             <h5 className='text-center mt-3'>Node.js</h5>
+             <p className='text-center text-sm skill-p'>Building scalable server-side applications</p>
+           </div>
+          </div>
+          <div className="col-md-3">
+          <div className="className='w-50 h-100 skill-box">
+             <img src={express} className='skill-width mx-auto d-block img-fluid mt-md-3' alt="" />
+             <h5 className='text-center mt-3'>Express.js</h5>
+             <p className='text-center text-sm skill-p'>Lightweight web framework for building robust APIs and applications.</p>
+           </div>
+          </div>
+          <div className="col-md-3">
+          <div className="className='w-50 h-100 skill-box">
+             <img src={mongo} className='skill-width mx-auto d-block img-fluid mt-md-3' alt="" />
+             <h5 className='text-center mt-3'>MongoDB</h5>
+             <p className='text-center text-sm skill-p'>NoSQL database for storing and managing high-volume data efficiently.</p>
+           </div>
+          </div>
+          <div className="col-md-3">
+          <div className="className='w-50 h-100 skill-box">
+             <img src={firebase} className='skill-width mx-auto d-block img-fluid mt-md-3' alt="" />
+             <h5 className='text-center mt-3'>Firebase</h5>
+             <p className='text-center text-sm skill-p'>Backend-as-a-Service for fast app development</p>
+           </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+          <div className="className='w-50 h-100 skill-box">
+             <img src={redux} className='skill-width mx-auto d-block img-fluid mt-md-3' alt="" />
+             <h5 className='text-center mt-3'>Redux</h5>
+             <p className='text-center text-sm skill-p'>State management for React applications</p>
+           </div>
+          </div>
+          <div className="col-md-3">
+          <div className="className='w-50 h-100 skill-box">
+             <img src={git} className='skill-width mx-auto d-block img-fluid mt-md-3' alt="" />
+             <h5 className='text-center mt-3'>Git</h5>
+             <p className='text-center text-sm skill-p'>Version control for collaborative software development</p>
+           </div>
+          </div>
+          <div className="col-md-3">
+          <div className="className='w-50 h-100 skill-box">
+             <img src={figma} className='skill-width mx-auto d-block img-fluid mt-md-3' alt="" />
+             <h5 className='text-center mt-3'>Figma</h5>
+             <p className='text-center text-sm skill-p'>UI/UX design and prototyping tool</p>
+           </div>
+          </div>
+          <div className="col-md-3"></div>
+        </div>
+      </div>
     </div>
   )
 }
+
